2be12ddcb11f73bac5eece59dcff836444420435
giupt <86053202+giupt@users.noreply.github.com>
2021-06-18 18:16:33 +0200

    Create OracleInterface2.sol

---
+++ OracleInterface2.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract OracleInterface {
+    
+    function getVerification(_id) public view returns (
+        string name, 
+        string project, 
+        uint date,   
+        uint result);
+
+    function testConnection() public pure returns (bool);
+
+    function addTestData() public; 
+}
