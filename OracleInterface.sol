e87095259f4310a029c69e8c40b31edf65e74e75
giupt <86053202+giupt@users.noreply.github.com>
2021-06-18 18:14:44 +0200

    Update OracleInterface.sol

--- OracleInterface.sol
+++ OracleInterface.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.4.17;
 
 contract OracleInterface {
  
+    function matchExists(bytes32 _matchId) public view returns (bool); 
+    
     function getVerification(bytes32 _verificationId) public view returns (
         string name, 
         string project, 
