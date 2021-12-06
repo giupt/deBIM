472a8fc39fc91e9c7c72f55788fbd2a3ef3d75c9
giupt <86053202+giupt@users.noreply.github.com>
2021-12-06 18:35:17 +0000

    Update OracleInterface3.sol

--- OracleInterface3.sol
+++ OracleInterface3.sol
@@ -5,11 +5,11 @@ contract OracleInterface {
     mapping(bytes32 => uint) verificationIdToIndex;
 
     function getVerification(bytes32 _verificationId) public view returns (
-       bytes32 id;
-       string discipline;
-       string team;
-       uint cycle;
-       uint date;
+       bytes32 id,
+       string discipline,
+       string team,
+       uint cycle,
+       uint date,
        uint result);
 
     function testConnection() public pure returns (bool);
