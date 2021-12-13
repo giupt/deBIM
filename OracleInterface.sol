f9ed6a63ae6bed18bf3cddcde514410055c878a8
giupt <86053202+giupt@users.noreply.github.com>
2021-12-13 13:09:34 +0000

    Create OracleInterface.sol

---
+++ OracleInterface.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.0;
+
+contract OracleInterface {
+
+enum VerificationResult {
+        Pending,    //verification is under decision
+        Validated,  //verification has met the minimum level of compliance
+        Rejected    //verification hasn't met the minimum level of compliance
+    }
+
+    function verificationExists(bytes32 _verificationId) public view returns (bool); 
+   
+    function getVerification(bytes32 _verificationId) public view returns (
+       bytes32 id,
+        string discipline,
+        string team,
+        uint cycle,
+        uint date,
+        uint percentage,
+        VerificationResult result);
+
+    function testConnection() public pure returns (bool);
+
+    function addTestData() public; 
+}
